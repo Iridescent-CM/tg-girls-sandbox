446f75e4e09d30d4c50b213747491ca230d2e166
Vivian Canales <vcnales@gmail.com>
2021-08-05 16:19:28 -0500

    Added mobile responsive styling to contact us section

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -5,15 +5,15 @@
       <p>To learn more about our programs and research, howe we work with partners, or to create a partnership tailored to fit your organization's needs.</p>
     </section>
 
-    <section id="links" class="flex justify-between my-12">
-      <div id="icons" class="flex w-48 justify-between">
+    <section id="links" class="flex flex-wrap-reverse md:flex-nowrap justify-between my-12">
+      <div id="icons" class="flex mx-auto md:mx-0 my-8 md: my-0 w-1/2 md:w-48 justify-between">
         <img src="../assets/instagram.png" alt="" class="social-icon">
         <img src="../assets/fb.png" alt="" class="social-icon">
         <img src="../assets/twitter.png" alt="" class="social-icon">
         <img src="../assets/youtube.png" alt="" class="social-icon">
       </div>
 
-      <div id="site-links" class="flex flex-wrap w-1/2">
+      <div id="site-links" class="flex flex-wrap w-full  md:mx-0 md:w-1/2 my-8 md: my-0">
         <a href="#" class="site-link">FAQ</a>
         <a href="#" class="site-link">Contact</a>
         <a href="#" class="site-link">Terms of Use</a>
