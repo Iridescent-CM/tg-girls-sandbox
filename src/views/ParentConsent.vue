adca672c9a69bf59615451947f594d413308da26
Vivian Canales <vcnales@gmail.com>
2021-08-20 13:11:45 -0500

    Created parent consent component for future parent consent functionality

---
+++ src/views/ParentConsent.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="parent-consent-page">
+    <Banner banner-text="PARENT CONSENT"/>
+    <FlashMessage class="mb-20"/>
+
+    <div class="parent-container">
+      <h1 class="text-4xl font-bold text-left mb-6">Welcome to the Technovation Girls Community!</h1>
+      <div id="thick-rule" class="bg-energetic-blue h-1"></div>
+      <h1 class="text-4xl text-center mt-6 mb-3">Consent for Participation</h1>
+
+      <section id="parent-consent-letter" class="text-left">
+        <p>Dear Parent/Guardian(s)</p>
+        <p>Thank you for supporting your daughter to participate in Technovation Girls, the technology entrepreneurship program and completion for students who identify as female, trans, nonbinary or gender nonconforming offered by Technovation, a technology education non-profit.</p>
+        <p>During the program, your child will learn the basics of computer programming and create a mobile app that solves a problem in her community. She will learn to be a technology entrepreneur! Technovation Girls is more than a coding program; it is an opportunity for your child to develop a variety of life and professional skills, and to challenge herself.</p>
+        <p>It won't always be easy for her, but she will be encouraged to persist when she meets adversity and obstacles. Completing the Technovation Girls curriculum is a huge accomplishment for each participant. When she is done, she will have completed the early stages of founding a real-life, mobile app startup. Your child's learning is at the center of all that we do. We share your commitment to your daughter's success, and have a page to help you understand more about the Technovation program, and how you can help support your child's participation. We take safety seriously and invite you to review our safety guidelines and other helpful resources here.</p>
+        <p>Whether your daughter chooses to work in a team or on her own, we recommend that she be supported by a mentor (an educator, parent, or industry professional). In addition to completing Technovation's online curriculum, your child will be expected to submit her project's deliverables online. Your child can work and communicate with her mentor either online or at in-person sessions, following COVID-19 related safety guideliens in your area. Parents are always welcome to attend any sessions and monitor all communications - here is a sample communication plan.</p>
+        <p>After your daughter's tem submits their idea, Technovation judges, who are technology professionals, will review the team's entry and provide feedback on their work. After the judging process, if you daughter is selected as a program finalist or winner, she and her team will be invited to attend the World Summit and present their pitch live to a panel of judges.</p>
+        <p>We are delighted to help foster your daughter's interest and development in technology and entrepreneurship and we are honored to work with students everywhere as they develop new skills and confidence.</p>
+        <p>If you have any questions, please don't hesitate to contact us at support@technovation.org</p>
+        <p>Warm Regards, <br> The Technovation Staff</p>
+      </section>
+
+      <div class="bg-energetic-blue h-1 mb-4"></div>
+
+      <div id="parent-consent-legal" class="text-left">
+        <section id="release-section">
+          <p><span class="font-bold">THIS RELEASE OF LIABILITY AND CONSENT AGREEMENT</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusamus aspernatur, dolore doloribus eligendi est laudantium nemo, nobis non obcaecati praesentium qui quos repellat sed sunt totam. Sed, voluptates! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto consectetur dicta dignissimos dolorum earum eveniet, illum iste libero, mollitia nam nemo placeat praesentium repellat reprehenderit sint temporibus velit veritatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem consequuntur cum, dignissimos doloremque eligendi excepturi harum id ipsa ipsum laudantium mollitia nulla optio perspiciatis rem, sapiente similique tempore voluptates!</p>
+        </section>
+
+        <section id="program-activities-section">
+          <p><span class="font-bold">1.Program Activities</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusamus aspernatur, dolore doloribus eligendi est laudantium nemo, nobis non obcaecati praesentium qui quos repellat sed sunt totam. Sed, voluptates! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto consectetur dicta dignissimos dolorum earum eveniet, illum iste libero, mollitia nam nemo placeat praesentium repellat reprehenderit sint temporibus velit veritatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem consequuntur cum, dignissimos doloremque eligendi excepturi harum id ipsa ipsum laudantium mollitia nulla optio perspiciatis rem, sapiente similique tempore voluptates!</p>
+        </section>
+
+        <section id="media-release-section">
+          <p class="mb-8"><span class="font-bold">18.Media Release</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusamus aspernatur, dolore doloribus eligendi est laudantium nemo, nobis non obcaecati praesentium qui quos repellat sed sunt totam. Sed, voluptates! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto consectetur dicta dignissimos dolorum earum eveniet, illum iste libero, mollitia nam nemo placeat praesentium repellat reprehenderit sint temporibus velit veritatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem consequuntur cum, dignissimos doloremque eligendi excepturi harum id ipsa ipsum laudantium mollitia nulla optio perspiciatis rem, sapiente similique tempore voluptates!</p>
+          <p><span class="font-bold">Technovation</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusamus aspernatur, dolore doloribus eligendi est laudantium nemo, nobis non obcaecati praesentium qui quos repellat sed sunt totam. Sed, voluptates! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto consectetur dicta dignissimos dolorum earum eveniet, illum iste libero, mollitia nam nemo placeat praesentium repellat reprehenderit sint temporibus velit veritatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem consequuntur cum, dignissimos doloremque eligendi excepturi harum id ipsa ipsum laudantium mollitia nulla optio perspiciatis rem, sapiente similique tempore voluptates!</p>
+        </section>
+
+        <section id="agreement">
+          <p class="font-bold">BY SIGNING, I A CKNOWLEDGE THEAT I HAVE READ AND UNDERSTOOD ALL OF THE TERMS OF THIS AGREEMENT AND THAT I AM VOLUNTARILY GIVING UP SUBSTANTIAL LEGAL RIGHTS, INCLUDING THE RIGHT TO SUE TECHNOVATION AND ITS AFFILIATES, ITS REPRESENTATIVES AND/OR THOSE INVOLVED IN THE PRODUCTION AND DISTRIBUTION OF THE ASSETS MENTIONED ABOVE. I ALSO ACKNOWLEDGE THAT BY SIGNING THIS DOCUMENT, I AM AGREEING TO ALL PARTS OF IT, INCLUDING THE MEDIA RELEASE.</p>
+        </section>
+      </div>
+
+      <div class="bg-energetic-blue h-1 mb-4"></div>
+
+      <section class="text-left mb-8">
+        <p>Student's name</p>
+        <h1 class="text-2xl font-bold">STUDENT'S NAME HERE</h1>
+
+        <form action="" class="mb-8">
+          <label for="yourName"></label>
+          <input type="text" id="yourName" name="yourName">
+        </form>
+        <button type="submit" class="consent-btn">I agree</button>
+      </section>
+
+    </div>
+
+    <div class="bg-energetic-blue h-6 my-16"></div>
+
+    <ContactUs/>
+
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner'
+import FlashMessage from "../components/parent_consent/FlashMessage";
+import ContactUs from "../components/ContactUs";
+export default {
+  name: 'ParentConsent',
+  components: {
+    Banner,
+    FlashMessage,
+    ContactUs
+  }
+}
+</script>
+
+<style>
+.parent-container{
+  @apply container mx-auto w-10/12 rounded-md;
+}
+
+#parent-consent-letter > p {
+  @apply mb-8
+}
+
+#parent-consent-legal > section {
+  @apply mb-8
+}
+
+#yourName{
+  @apply border-4 border-green-500 w-3/5
+}
+
+.consent-btn{
+  @apply px-4 py-2 rounded bg-green-500 text-white hover:bg-green-600
+}
+</style>
