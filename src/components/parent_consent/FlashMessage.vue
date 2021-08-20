51a0a2c612ea199eea34a164b304becdd53b690e
Vivian Canales <vcnales@gmail.com>
2021-08-20 13:11:12 -0500

    Created flash message component for parent-consent pages

---
+++ src/components/parent_consent/FlashMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="flash-message" class="w-10/12 text-white m-auto text-left p-4 rounded-lg">
+    <p class="mb-4">You must complete the following two forms before you daughter can participate in Technovation Girls</p>
+    <p class="text-xl">&#9744; Consent to participate in the program</p>
+    <p class="text-xl">&#9744; Media Release</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlashMessage"
+}
+</script>
+
+<style scoped>
+#flash-message{
+  background: #CA1240;
+
+
+}
+</style>
