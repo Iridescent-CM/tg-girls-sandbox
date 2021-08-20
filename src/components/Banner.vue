4f48e6d8a9c5a932e375fb94d4f78ee7dfd2c607
Vivian Canales <vcnales@gmail.com>
2021-08-20 13:12:23 -0500

    Updated Banner component to be dynamic in order to change banner text

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="tg-green-stripes rubik h-36 flex items-center mb-20">
-    <div class="text-white sm:ml-8 lg:ml-72 text-7xl font-black">PROFILE SETUP</div>
+    <div class="text-white sm:ml-8 lg:ml-72 text-7xl font-black">{{ bannerText }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Banner',
+  props: {
+    bannerText: String
+  }
+}
+</script>
